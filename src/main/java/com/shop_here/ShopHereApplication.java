05013923958ebe0ef63fdeac6af15fbb326532e9
keshavsaini137 <keshavsaini137@gmail.com>
2025-10-30 07:06:00 +0530

    Initial commit

---
+++ src/main/java/com/shop_here/ShopHereApplication.java
@@ -0,0 +1,13 @@
+package com.shop_here;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopHereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop_here/ShopHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop_here;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopHereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
